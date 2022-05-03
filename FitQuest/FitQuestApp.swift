5a33334064ec47df1888bc9d6fd09a4720b01dc7
Alberto <aldo10012@gmail.com>
2022-05-02 19:17:41 -0700

    first commit

---
+++ FitQuest/FitQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitQuestApp.swift
+//  FitQuest
+//
+//  Created by Alberto Dominguez on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FitQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
