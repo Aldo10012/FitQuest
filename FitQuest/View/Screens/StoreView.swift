e2be8471e288d23d60ee4cfbae7f899e8f60f1c7
Alberto <aldo10012@gmail.com>
2022-05-11 13:02:01 -0700

    [ADDED] StoreView item gallery

--- FitQuest/View/Screens/StoreView.swift
+++ FitQuest/View/Screens/StoreView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct StoreView: View {
+    
+    let itemStore = ItemStore()
+    
+    let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+    
     var body: some View {
         NavigationView {
-            Text("Hello, World!")
-                .navigationBarTitle(Text("Shop"), displayMode: .inline)
+            VStack {
+                HStack() {
+                    Text("Level \(1)")
+                    Spacer()
+                    Text("Coins \(10)")
+                }
+                .padding(.horizontal)
+                
+                ScrollView {
+                    ItemGalleryView(label: "Weapons", itemList: itemStore.weaponsInventory)
+                    ItemGalleryView(label: "Armor", itemList: itemStore.armorInventory)
+                    ItemGalleryView(label: "Special Items", itemList: itemStore.specialItemsInventory)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            }
+            .navigationBarTitle(Text("Shop"), displayMode: .inline)
         }
-        
     }
 }
 
